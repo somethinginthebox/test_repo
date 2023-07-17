f4356c4c9dae900158f16b7e429c1ce46dae56f2
somethinginthebox <ha862888@gmail.com>
2023-07-17 18:45:13 +0900

    test 3

---
+++ app/src/main/java/com/example/myapplication/WorldMain.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+import android.util.Log
+
+fun main(){
+    println("hello world")
+    print("안녕하세요")
+    print("push")
+}
+https://github.com/somethinginthebox/test_repo.git
